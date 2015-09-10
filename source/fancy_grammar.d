ed67fc8dfd80eff2076319594adbe9a2cf8a0b20
Stefan Koch <uplink.coder@gmail.com>
2015-09-10 16:44:59 +0200

    Add grammer analyzer. It basicly caches information about the grammar. So it does not need to be recomputed in each component of fancyPars. Though... due to cache effects recomputing maybe faster...

--- source/fancy_grammar.d
+++ source/fancy_grammar.d
@@ -470,3 +470,158 @@ bool isASTMember(const PatternElement element) {
 
 	return (ne || fe || re || nc /*|| ce*/);
 }
+
+final class GrammerAnalyzer {
+	Group root;
+	bool isAnalyzed;
+	
+	this(const Group root) {
+		this.root = cast(Group) root;
+	}
+	
+	static struct AnalyzedGrammar {
+		string rootNodeName;
+		SortedRange!(string[], "a < b") strings;
+		
+		static struct GroupInformation {
+			bool isRoot;
+			bool hasGroups;
+			bool isRangeGroup;
+			bool isDelimitedCharGroup;
+			Group directLeftRecursiveParent;
+			Group[] directLeftRecursiveChildren;
+			
+			Group parent;
+		}
+		
+		static struct ElementInformation {
+		pure :
+			void analyzeElement(PatternElement elm, ref AnalyzedGrammar ag, PatternElement parent = null) {
+				parentElement = &parent;
+				
+				with (ElementTypeEnum) {
+					if (!cast(LexerElement)elm) {
+						if (auto e = cast(AlternativeElement) elm) {
+							eType = _AlternativeElement;
+							containingElements = e.alternatives;
+						} else if (auto e = cast(FlagElement) elm) {
+							eType = _FlagElement;
+						} else if (auto e = cast(NamedElement) elm) {
+							eType = _NamedElement;
+							containingElements = [e.lst_sep];
+						} else if (auto e = cast(OptionalElement) elm) {
+							eType = _OptionalElement;
+							containingElements = *(cast(PatternElement[]*)(&e.ce)) ~ e.elem;
+						} else if (auto e = cast(ParenElement) elm) {
+							eType = _ParenElement;
+							containingElements = e.elements;
+						} else if (auto e = cast(QueryElement) elm) {
+							eType = _QueryElement;
+							containingElements = [e.elem];
+						}
+					} else {
+						if (auto e = cast(LookbehindElement) elm) {
+							eType = _LookbehindElement;
+						} else if (auto e = cast(NamedChar) elm) {
+							eType = _NamedChar;
+						} else if (auto e = cast(NotElement) elm) {
+							eType = _NotElement;
+							containingElements = [e.ce];
+						} else if (auto e = cast(RangeElement) elm) {
+							eType = _RangeElement;
+							//} else if (auto e = cast(PeekElement) elm) {
+							//eType =	_PeekElement;
+							//containingElements = [e.ce];
+						} else if (auto e = cast(StringElement) elm) {
+							eType = _StringElement;
+						}
+					}
+					
+					if (containingElements) {
+						foreach(ce;containingElements) {
+							ElementInformation cei;
+							cei.analyzeElement(ce, ag, elm);
+							ag.elementInformation[ce] = cei;
+						}
+					}
+				}
+				
+			}
+			
+			Group* parentGroup;
+			PatternElement* parentElement;
+			
+			enum ElementTypeEnum {
+				_Invalid = 0,
+				_AlternativeElement,
+				_FlagElement,
+				_NamedElement,
+				_OptionalElement,
+				_ParenElement,
+				_QueryElement,
+				// LezerElements
+				_LookbehindElement,
+				_NamedChar,
+				_NotElement,
+				_RangeElement,
+				_PeekElement,
+				_StringElement,
+			}
+			
+			ElementTypeEnum eType;
+			PatternElement[] containingElements;
+			
+			bool isASTMember() {
+				with (ElementTypeEnum)
+					return eType == _NamedElement 
+						|| eType == _NamedChar 
+						|| eType == _RangeElement
+						|| eType == _FlagElement;
+			}
+		}
+		
+		const(Group)[const(char[])] groupsByName;
+		const(GroupInformation)[const Group] groupInformation;
+		const(ElementInformation)[const PatternElement] elementInformation;
+	}
+	
+	AnalyzedGrammar analyze() {
+		AnalyzedGrammar result;
+		auto allGroups = root.getAllGroups;
+		result.strings = allGroups.getStrings;
+		result.rootNodeName = root.name.identifier;
+		
+		Group[] getAllGroups(Group root, Group parent = null) {
+			Group[] _result;
+			AnalyzedGrammar.GroupInformation gi;
+			import std.exception:enforce;
+			enforce (root.name.identifier !in result.groupsByName, "multiple groups are named "
+				~ root.name.identifier);
+			result.groupsByName[root.name.identifier] = root;
+			if (parent) {
+				gi.parent = parent;
+			}
+			if (root.hasGroups) {
+				_result ~= root;
+				
+				foreach(gr;root.groups) {
+					_result ~= getAllGroups(gr, root);
+				}
+				
+			} else {
+				foreach(el;root.elements) {
+					AnalyzedGrammar.ElementInformation ei;
+					ei.parentGroup = &root;
+					ei.analyzeElement(el, result);
+					result.elementInformation[el] = ei;
+				}
+			}
+			
+			result.groupInformation[root] = gi;
+			return _result;
+		}
+		
+		return result;
+	}
+	
+}
