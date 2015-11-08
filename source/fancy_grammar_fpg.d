128a8a7158d09133f1c614291b3dab1916b259a0
Stefan Koch <uplink.coder@gmail.com>
2015-11-08 01:13:50 +0100

    Add the Grammar

---
+++ source/fancy_grammar_fpg.d
@@ -0,0 +1,74 @@
+﻿module fancy_grammar_fpg;
+
+static immutable string fancyParsGrammar  = `ASTNode {
+    Identifier @internal {
+        [a-zA-Z_][] identifier
+    }
+
+    Group @parent {
+        Identifier name, ? "@" : Identifier[] annotations : "@", "{",
+            PatternElement[] elements : "," / Group[] groups,
+             "}"
+    }   
+
+    PatternElement @internal {
+
+        AlternativeElement @noFirst {
+            PatternElement[] alternatives : "/"
+        }
+		
+		AnonymosuGroupElement {
+			"{", PatternElement[] elements : ",", "}"
+		}
+		
+        LexerElement {
+
+            StringElement {
+                "\"", char[] string_, "\""
+            }
+
+            NamedChar {
+                "char", ? "[]" : bool isArray, Identifier name
+            }
+
+            CharRange @internal {
+                char rangeBegin,  ? "-" : char RangeEnd
+            }
+
+            RangeElement {
+                "[", CharRange[] ranges, "]"
+            }
+
+            LookbehindElement {
+                "?lb", "(", StringElement str, ")"
+            }
+
+            NotElement {
+                "!", LexerElement ce
+            }
+
+        }
+
+        NamedElement {
+            Identifier type / AnonymousGroupElement age,  ? "[]" : bool isArray, Identifier name, 
+            ? bool isArray : ? ":" : StringElement lst_sep
+        }
+
+        ParenElement {
+            "(", PatternElement[] elements : ",", ")" 
+        }
+
+        FlagElement {
+            "bool", Identifier flag_name
+        }
+
+        QueryElement {
+            "?", "bool", Identifier flag_name, ":", PatternElement elem
+        }
+
+        ConditionalElement {
+            "?", LexerElement[] ce : ",", ":", PatternElement elem
+ 		}
+
+    }
+}`;
