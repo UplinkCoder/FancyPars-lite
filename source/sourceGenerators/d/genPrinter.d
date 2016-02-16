6dcb55ef6cf8a4ea824831b0fb4ee87f596cd974
Stefan Koch <uplink.coder@googlemail.com>
2016-02-16 13:37:25 +0000

    WIP enmuify for printer

--- source/sourceGenerators/d/genPrinter.d
+++ source/sourceGenerators/d/genPrinter.d
@@ -3,6 +3,71 @@ import fancy_ast;
 import fancy_analyzer;
 import fancy_util;
 import sourceGenerators.d.genVisitor;
+import fancy_grammar_patterns;
+
+string enumifiy(const AnalyzedGrammar ag, const EnumifiableGroup_ eg) pure {
+	string result;
+
+
+
+	return result;
+}
+
+string genPtrStmt(const PatternElement e) pure {
+	string result;
+	
+	if (auto se = cast(StringElement)e) {
+		result ~= "sink.put(\"".indentBy(3) ~ se.string_ ~ "\");\n";
+	} else if (auto oe = cast(ConditionalElement)e) {
+		if (oe.elem.isASTMember) {
+			
+			result ~= "if (g.".indentBy(3) ~ getName(oe.elem) 
+				~ ") {\n";
+			foreach(le;oe.ce) { 
+				result ~= genPtrStmt(le);
+			}
+			result ~= genPtrStmt(oe.elem)
+				~ "}\n".indentBy(3);
+		}
+	} else if (auto alte = cast(AlternativeElement)e) {
+		foreach (alt;astMembers(alte)) {
+			result ~= "if (g.".indentBy(3) ~ getName(alt) 
+				~ ") {\n" ~ genPtrStmt(alt) 
+					~ "}".indentBy(3);
+		}
+	} else if (auto ne = cast(NamedElement)e) {
+		if (ne.isArray) {
+			result ~= "foreach(_e;".indentBy(3)
+				~ "g." ~ getName(e) ~ ") {\n"
+					~	"print(_e".indentBy(4) 
+					~ ");\n";
+			if (ne.lst_sep) {
+				result ~= "sink.put(\"".indentBy(4)
+					~ ne.lst_sep.string_ ~ "\");\n";
+			}
+			result ~= "}".indentBy(3) ~ "\n";
+		} else {
+			result ~= "print(g.".indentBy(3) 
+				~ getName(e) ~ ");\n";
+		} 
+	} else if (auto ce = cast(NamedChar)e) {
+		if (ce.isArray) {
+			result ~= "foreach(_e;".indentBy(3)
+				~ "g." ~ getName(e) ~ ") {\n"
+					~	"print(_e".indentBy(4) 
+					~ ");\n";
+			result ~= "}".indentBy(3) ~ "\n";
+		} else {
+			result ~= "print(g.".indentBy(3) 
+				~ getName(e) ~ ");\n";
+		} 
+	} else {
+		//assert(0);
+	}
+	
+	
+	return result;
+}
 
 string genPrinter (const GrammerAnalyzer.AnalyzedGrammar ag) pure {
 	string blrplate=`
@@ -18,62 +83,6 @@ string print(const `~ ag.allGroups[0].name.identifier ~` root) pure {
 `;
 	string result;
 
-	string genPtrStmt(const PatternElement e) {
-		string result;
-
-		if (auto se = cast(StringElement)e) {
-			result ~= "sink.put(\"".indentBy(3) ~ se.string_ ~ "\");\n";
-		} else if (auto oe = cast(ConditionalElement)e) {
-				if (oe.elem.isASTMember) {
-
-				result ~= "if (g.".indentBy(3) ~ getName(oe.elem) 
-					~ ") {\n";
-				foreach(le;oe.ce) { 
-					result ~= genPtrStmt(le);
-				}
-				result ~= genPtrStmt(oe.elem)
-					~ "}\n".indentBy(3);
-			}
-			} else if (auto alte = cast(AlternativeElement)e) {
-				foreach (alt;astMembers(alte)) {
-					result ~= "if (g.".indentBy(3) ~ getName(alt) 
-						~ ") {\n" ~ genPtrStmt(alt) 
-						~ "}".indentBy(3);
-				}
-			} else if (auto ne = cast(NamedElement)e) {
-				if (ne.isArray) {
-				result ~= "foreach(_e;".indentBy(3)
-				~ "g." ~ getName(e) ~ ") {\n"
-				~	"print(_e".indentBy(4) 
-				~ ");\n";
-				if (ne.lst_sep) {
-					result ~= "sink.put(\"".indentBy(4)
-					~ ne.lst_sep.string_ ~ "\");\n";
-				}
-				result ~= "}".indentBy(3) ~ "\n";
-			} else {
-				result ~= "print(g.".indentBy(3) 
-					~ getName(e) ~ ");\n";
-			} 
-		} else if (auto ce = cast(NamedChar)e) {
-			if (ce.isArray) {
-				result ~= "foreach(_e;".indentBy(3)
-					~ "g." ~ getName(e) ~ ") {\n"
-						~	"print(_e".indentBy(4) 
-						~ ");\n";
-				result ~= "}".indentBy(3) ~ "\n";
-			} else {
-				result ~= "print(g.".indentBy(3) 
-					~ getName(e) ~ ");\n";
-			} 
-		} else {
-			//assert(0);
-		}
-				
-
-		return result;
-	}
-
 	foreach(eG;ag.allGroups.getElementGroups) {
 		result ~= "\n\n" ~ "void print(".indentBy(2)
 			~ eG.name.identifier ~ " g) {\n";
@@ -84,7 +93,7 @@ string print(const `~ ag.allGroups[0].name.identifier ~` root) pure {
 		result ~= "}\n".indentBy(2);
 	}
 
-	return  blrplate ~ genVisitor(ag.allGroups,"void","print",2) ~ result  ~ "\n\t}"
+	return  blrplate ~ genVisitor(ag, "void", "print", 2, &genPtrStmt) ~ result  ~ "\n\t}"
 		~ "\n\tauto ptr = Printer();\n\tptr.print(cast(" ~ ag.allGroups[0].name.identifier ~ 
 		")root);\n\treturn cast(string)ptr.sink.data;"
 			~ "\n}\n";
--- source/sourceGenerators/d/genVisitor.d
+++ source/sourceGenerators/d/genVisitor.d
@@ -3,11 +3,28 @@ module sourceGenerators.d.genVisitor;
 import fancy_ast;
 import fancy_analyzer;
 import fancy_util;
+import fancy_grammar_patterns;
 
-string genVisitor(const (Group)[] allGroups, const char[] retType = "void", const char[] funcName = "visit", uint iLvl=0) pure {
+string genVisitor(const AnalyzedGrammar ag, const char[] retType = "void", const char[] funcName = "visit", uint iLvl=0, string function(const PatternElement) pure ef = null) pure {
+	auto allGroups = ag.allGroups;
+	const(Group)[] excludeGroups;
 	string result;
 
 	foreach(pG;allGroups.getParentGroups) {
+		if (auto eg = EnumifiableGroup(pG)) {
+			result ~= "\n\n" ~ retType.indentBy(iLvl) ~ " " ~ funcName ~ "(" 
+				~ pG.name.identifier ~ "Enum _e) {\n" ~ "switch (_e) {\n".indentBy(iLvl + 1);
+
+				foreach(entry;eg.enumEntries) {
+					result ~= "case ".indentBy(iLvl + 2) ~ pG.name.identifier ~  "Enum." ~ entry.enumName.identifier ~ " :\n"
+						~ ef(entry.enumValue).indentBy(iLvl);
+					excludeGroups ~= ag.allGroups.getGroupNamed(entry.enumName.identifier);
+				}
+
+				result ~= "\n" ~ "}\n".indentBy(iLvl + 1) ~ "}\n".indentBy(iLvl);
+				continue ;
+		}
+
 		result ~= "\n\n" ~ retType.indentBy(iLvl) ~ " " ~ funcName ~"(" ~ pG.name.identifier ~ " _p) {\n"
 			~ "".indentBy(iLvl+1);
 		foreach (eG;pG.groups) {
@@ -16,5 +33,13 @@ string genVisitor(const (Group)[] allGroups, const char[] retType = "void", cons
 		}
 		result = cast(string)(result[0..$-5] ~ "\n" ~ "}".indentBy(iLvl));
 	}
+
+//	foreach(eG;allGroups.getElementGroups) {
+//		import std.algorithm;
+//		import std.array;
+//		if (!excludeGroups.canFind!((a,b)=> a is b)(eG)) {
+//			result ~= eG.elements.map!(eG => ef(eG)).array.join;
+//		}
+//	}
 	return result;
 }
