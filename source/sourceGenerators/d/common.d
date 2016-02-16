a516479ef7b5ad44627f9c3bb3c2652068c772be
Stefan Koch <uplink.coder@googlemail.com>
2016-02-16 10:36:23 +0000

    better CTFE error message

--- source/sourceGenerators/d/common.d
+++ source/sourceGenerators/d/common.d
@@ -1,12 +1,13 @@
 module sourceGenerators.d.common;
 import fancy_ast;
+import fancy_util;
 import fancy_analyzer;
 import std.conv;
 
 string genDecl(const PatternElement element) pure {
 	string result;
 	if(!element.isASTMember) {
-		assert(0, "Declarations can only be generated for AST-Memebrs");
+		assert(0, "Declarations can only be generated for AST-Memebrs " ~  (__ctfe ? (unQual(element)).to!string : to!string(typeid(element))) );
 	}
 	
 	return getType(element) ~ " " ~ getName(element) 
