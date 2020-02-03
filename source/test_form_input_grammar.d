0b9e0823534e72ab84a08ead96d4707c8d13bf86
Stefan Koch <uplink.coder@googlemail.com>
2020-02-03 22:53:40 +0100

    Update and rename test.fpg to test_form_input_grammar.d

--- source/test_form_input_grammar.d
+++ source/test_form_input_grammar.d
@@ -1,3 +1,4 @@
+enum grammer = `
 RootNode
 {
     Desc @parent {
@@ -33,3 +34,17 @@ RootNode
 	}
 	
 }
+`;
+
+enum ExampleCode = `
+	FormInput
+	{
+		name : string;
+		age : int32
+	}
+	if (age is null) { addToBody("FormInput age is expected"); }
+	if (age not in  0 .. 99)
+	{
+		addToBody("age: %d is not in the range from 0 to 99< br/>", age);
+	}
+`;
